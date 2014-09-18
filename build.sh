c35a74621fd82821654b3128e0278a6fbb267ecb
Brian Shore <brian@networkredux.com>
2014-09-18 18:24:18 +0000

    don't hardcode rpm path as ~/rpmbuild

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 whereami=$(dirname $0)
+readonly TOPDIR=`rpm --eval '%{_topdir}'`
 
 if [ ! -f "/etc/rpm/macros.dist" ] && \
    [ ! -f "/etc/rpm/macros.disttag" ];   then echo "please install 'buildsys-macros' rpm and try again" ; exit 1 ; fi
@@ -8,12 +9,12 @@ if [ ! -f "$(which rpmbuild)" ];         then echo "please install 'rpm-build' r
 if [ ! -f "$(which spectool)" ];         then echo "please install 'rpmdevtools' rpm and try again" ; exit 1 ; fi
 if [ ! -f "$(which rpmdev-setuptree)" ]; then echo "please install 'rpmdevtools' rpm and try again" ; exit 1 ; fi
 
-# creates ~/rpmbuild
+# creates build tree
 /usr/bin/rpmdev-setuptree
 
-cp -f ${whereami}/runit.spec ~/rpmbuild/SPECS/
-cp -f ${whereami}/*.patch    ~/rpmbuild/SOURCES/
-cp -f ${whereami}/runsvdir-start.service    ~/rpmbuild/SOURCES/
-/usr/bin/spectool -C ~/rpmbuild/SOURCES/ -g ${whereami}/runit.spec 
+cp -f ${whereami}/runit.spec $TOPDIR/SPECS/
+cp -f ${whereami}/*.patch    $TOPDIR/SOURCES/
+cp -f ${whereami}/runsvdir-start.service    $TOPDIR/SOURCES/
+/usr/bin/spectool -C $TOPDIR/SOURCES/ -g ${whereami}/runit.spec 
 
-rpmbuild -bb ~/rpmbuild/SPECS/runit.spec
+rpmbuild -bb $TOPDIR/SPECS/runit.spec
